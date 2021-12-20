f2794e21c88319393ce5e7f7dbd0842fdd277a22
Lars <halbauer90@gmail.com>
2021-12-20 21:46:06 +0100

    Comparison for different parts added

--- lib/cncimport.py
+++ lib/cncimport.py
@@ -11,15 +11,16 @@ def clear_code(s):
     # replace all linebreaks
     lines = s.splitlines()
     # replace all indentations
-    lines = [l.lstrip() for l in lines]
+    lines = [l.strip() for l in lines]
     return lines
 
 
 def find_desired_section(textlist,
-                         start_string='START_EBH:',
+                         start_string='START_EBH:'.lower(),
                          end_string=None):
+    textlist = [s.lower() for s in textlist]
     if not end_string:
-        end_string = ['END_EBH:', 'RET']
+        end_string = ['END_EBH:'.lower(), 'RET'.lower()]
     try:
         start_idx = textlist.index(start_string)
     except ValueError:
@@ -49,9 +50,8 @@ def get_parameter_mode(cnc_list, crit='if _paket'):
         mode = 'single'
     return mode
 
-
 def get_parameters(cnc,
-                   p_s, p_e, c_s, c_e, p_m, c_m, ax_finder=None):
+                   p_s, p_e, c_s, c_e, p_m, c_m, n_p='NP-1', ax_finder=None):
     if not ax_finder:
         ax_dict = {'a': {
                         'r': r'\ba{1}=\S+',
@@ -76,13 +76,36 @@ def get_parameters(cnc,
     else:
         ax_dict = ax_finder
 
-    contour = [s.lower() for s in cnc[c_s:c_e]]
+    if n_p == 'NP-1':
+        n_search = 'if _paket == "NP2"'.lower()
+    elif n_p == 'NP-2':
+        n_search = 'if _paket == "NP1"'.lower()
+    else:
+        raise AttributeError(f"Bauteil {n_p} unbekannt.")
+
+    para_section = cnc[p_s:p_e]
+    cnc_section = cnc[c_s:c_e]
+    if p_m == 'dual':
+        pstart, pend = find_desired_section(para_section, start_string=n_search, end_string=['ENDIF'.lower()])
+        not_p_np_s = p_s + pstart
+        not_p_np_e = p_s + pend
+
+    if c_m == 'dual':
+        cstart, cend = find_desired_section(cnc_section, start_string=n_search, end_string=['ENDIF'.lower()])
+        not_c_np_s = c_s + cstart
+        not_c_np_e = c_s + cend
+
+    contour = [s.lower() for s in cnc_section]
     exp = r'|'.join([v['r'] for k, v in ax_dict.items()])
     erg = [e for e in (re.findall(exp, c) for c in contour) if e]
 
     for k in ax_dict.keys():
         ax_dict[k]['val'] = [0] * len(erg)
         for i, stringlist in enumerate(erg):
+            if c_m == 'dual':
+                if not_c_np_s <= i <= not_c_np_e:
+                    continue
+
             for s in stringlist:
                 if re.match(ax_dict[k]['r'], s):
                     ax_dict[k]['val'][i] = re.sub(f'^{k}[= ]', '', s)
@@ -94,6 +117,8 @@ def get_parameters(cnc,
 
 if __name__ == "__main__":
     filename = "../data/EBH_347_BS.MPF"
+    nst = 'NP-1'
+
 
     raw_cnc = import_cnc(filename)
     cnc = clear_code(raw_cnc)
@@ -101,7 +126,7 @@ if __name__ == "__main__":
     strt, end = find_desired_section(cnc)
     ebh_cnc = cnc[strt:end]
 
-    c_strt, c_end = find_desired_section(ebh_cnc, start_string='PYR_STRT', end_string=['PYR_STOP'])
+    c_strt, c_end = find_desired_section(ebh_cnc, start_string='PYR_STRT'.lower(), end_string=['PYR_STOP'.lower()])
 
     par_cnc = cnc[strt:strt+c_strt]
     par_mode = get_parameter_mode(par_cnc)
@@ -109,7 +134,7 @@ if __name__ == "__main__":
     contour_cnc = cnc[strt+c_strt:strt+c_end]
     contour_mode = get_parameter_mode(contour_cnc)
 
-    get_parameters(cnc, strt, strt+c_strt, strt+c_strt, strt+c_end, par_mode, contour_mode)
+    get_parameters(cnc, strt, strt+c_strt, strt+c_strt, strt+c_end, par_mode, contour_mode, n_p=nst)
 
 
 
